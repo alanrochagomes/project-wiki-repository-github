5e7b2c54a88ab5884d9e30b7ad1491040072305e
Alan Rocha <alanrochaarg2001@gmail.com>
2025-06-03 11:55:51 -0300

    first commit

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import logo from '../assets/github.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} width={72} height={72} alt="logo" />
+     
+    </div>
+  );
+}
+
+export default App;
