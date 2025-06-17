00038970ac55d298e961c7e503f72da1fe4f9e07
Alan Rocha <alanrochaarg2001@gmail.com>
2025-06-17 13:55:50 -0300

    Criando Componente `Input`

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,13 @@
-import logo from '../assets/github.png';
-import { Container } from './styles';
+import logo from "../assets/github.png";
+import Input from "../components/Input";
 
+import { Container } from "./styles";
 
 function App() {
   return (
     <Container>
       <img src={logo} width={72} height={72} alt="github logo" />
-
+      <Input />
     </Container>
   );
 }
